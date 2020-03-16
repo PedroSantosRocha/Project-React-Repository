08cc24774e78cb35799d11d690a5b2bb8b1ef37c
PedroSantosRocha <pedrosantos.ti.web@gmail.com>
2020-03-16 00:51:40 -0300

    t

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 
 export const Container = styled.div`
   max-width: 700px;
@@ -13,17 +13,59 @@ export const Container = styled.div`
     display: flex;
     flex-direction: row;
     align-items: center;
-  }
 
-  svg {
-    margin-right: 10px;
+    svg {
+      margin-right: 10px;
+    }
   }
 `;
 
 export const Form = styled.form`
-  
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
+
+  input {
+    flex: 1;
+    border: 1px solid #EEE;
+    padding: 10px 15px;
+    border-radius: 4px;
+    font-size: 16px;
+  }
 `;
 
-export const SubmitButton = styled.button`
-  
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const SubmitButton = styled.button.attrs(props => ({
+  type: 'submit',
+  disabled: props.loading,
+}))`
+  background: #7951c1;
+  border: 0;
+  padding: 0 15px;
+  margin-left: 10px;
+  border-radius: 4px;
+
+  display: flex;
+  justify-content: center;
+  align-itens: center;
+
+  &[disabled] {
+    cursor: not-allowed;
+    opacity: 0.6;
+  }
+
+  ${props => props.loading && css`
+    svg {
+      animation: ${rotate} 2s linear infinite;
+    }
+  `}
 `;
